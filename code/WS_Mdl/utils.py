e016842fe6f16e8e8ff66757d8b33df22079b7b7
Marios <m.karampasis@uu.nl>
2025-09-05 14:36:37 +0200

    functinality to read MSW .inp files

--- code/WS_Mdl/utils.py
+++ code/WS_Mdl/utils.py
@@ -1,4 +1,5 @@
 # ***** Utility functions to facilitate more robust modelling. *****
+import json
 import os
 import re
 import shutil as sh
@@ -403,6 +404,41 @@ def MF6_block_to_DF(
         DF.columns = [f'col_{i}' for i in range(DF.shape[1])]
 
     return DF
+
+
+def MSW_In_to_DF(
+    Pa,
+):
+    Fi = PBN(Pa)
+
+    d_headers = json.load(open(PJ(Pa_WS, 'code/WS_Mdl/Auxi/MSW_headers.json')))
+    d_colspecs = json.load(open(PJ(Pa_WS, 'code/WS_Mdl/Auxi/MSW_colspecs.json')))
+
+    try:
+        if Fi == 'mete_grid.inp':
+            DF = pd.read_csv(Pa, header=None, names=d_headers[Fi])
+        elif Fi in d_colspecs:
+            DF = pd.read_fwf(Pa, colspecs=d_colspecs[Fi], header=None)  # , l_headers=d_headers[Fi])
+            DF.columns = d_headers[Fi][: DF.shape[1]]
+        elif Fi == 'para_sim.inp':
+            DF = pd.DataFrame({'Line': [l.strip() for l in open('MSW_In/para_sim.inp') if '=' in l]})
+            DF['parameter'] = DF['Line'].apply(lambda x: x.split('=')[0].strip())
+            DF['value'] = DF['Line'].apply(lambda x: x.split('=')[1].split('!')[0].strip() if '=' in x else '')
+            DF['comment'] = DF['Line'].apply(lambda x: x.split('!')[1].strip() if ('=' in x) and ('!' in x) else '')
+            DF.drop(columns=['Line'], inplace=True)
+        elif Fi == 'sel_key_svat_per.inp':
+            DF = pd.DataFrame({'Line': [l.strip() for l in open('MSW_In/sel_key_svat_per.inp')]})
+            DF['parameter'] = DF['Line'].apply(lambda x: x.split()[0])
+            DF['value'] = DF['Line'].apply(lambda x: x.split()[1])
+            DF.drop(columns=['Line'], inplace=True)
+        else:
+            DF = pd.read_fwf(Pa, header=None)  # , l_headers=d_headers[i]
+            DF.columns = d_headers[Fi][: DF.shape[1]]
+        vprint(Fi, '🟢')
+        return DF
+    except Exception as e:
+        vprint(Fi, '🔴', e)
+        return
 
 
 # --------------------------------------------------------------------------------
