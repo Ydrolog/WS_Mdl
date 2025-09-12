a97a906d265a1d34709c77206f3c8f13b8293efc
Marios <m.karampasis@uu.nl>
2025-09-12 15:01:02 +0200

    SFRmaker #1: Identifying closing erros in WBD SHP

--- code/WS_Mdl/calcs.py
+++ code/WS_Mdl/calcs.py
@@ -3,17 +3,19 @@ import numpy as np
 from sklearn.metrics import mean_squared_error
 
 
+def c_Dist(x1, y1, x2, y2):
+    """Calculate the Euclidean distance between two points."""
+    return np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+
 class Vld_Mtc:
     formulas = {
         'NSE': lambda obs, sim: 1 - (np.sum((obs - sim) ** 2) / np.sum((obs - np.mean(obs)) ** 2)),
         'RMSE': lambda obs, sim: np.sqrt(mean_squared_error(obs, sim)),
         'MAE': lambda obs, sim: np.mean(np.abs(obs - sim)),
-        'Correlation': lambda obs, sim: np.corrcoef(obs, sim)[
-            0, 1
-        ],  # Pearson correlation coefficient
+        'Correlation': lambda obs, sim: np.corrcoef(obs, sim)[0, 1],  # Pearson correlation coefficient
         'Bias Ratio': lambda obs, sim: np.mean(sim) / np.mean(obs),  # β = mean(sim) / mean(obs)
-        'Variability Ratio': lambda obs, sim: (np.std(sim) / np.mean(sim))
-        / (np.std(obs) / np.mean(obs)),  # γ'
+        'Variability Ratio': lambda obs, sim: (np.std(sim) / np.mean(sim)) / (np.std(obs) / np.mean(obs)),  # γ'
         'KGE': lambda obs, sim: 1
         - np.sqrt(
             (np.corrcoef(obs, sim)[0, 1] - 1) ** 2
