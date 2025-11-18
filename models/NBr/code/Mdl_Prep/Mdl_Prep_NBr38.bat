e2688b2bfe8f568576c7e25ace5b7ff2d56e3821
Marios <m.karampasis@uu.nl>
2025-11-18 10:40:24 +0100

    SFRmaker_NBr38: Run went through, but the output isn't much improved as I expected.

---
+++ models/NBr/code/Mdl_Prep/Mdl_Prep_NBr38.bat
@@ -0,0 +1,15 @@
+@echo off
+pushd %~dp0
+
+:: Set the variable
+set "SimN=NBr38"
+
+:: Check if the folder exists, if not, create it
+if not exist "..\..\Sim\%SimN%" (
+    mkdir "..\..\Sim\%SimN%"
+)
+
+:: Run the executable with the ini file
+"..\..\..\..\software\iMOD5\imod_r.exe" ".\Mdl_Prep_%SimN%.ini"
+
+REM EXIT
