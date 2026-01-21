a858613f7ebeb324c323ffa711c7c507f6606d06
Marios <m.karampasis@uu.nl>
2026-01-21 15:29:13 +0100

    split Upl_software to Upl_installers and Upl_iMOD5

--- code/Py/iBridges/software/Upl_iMOD5.py
+++ code/Py/iBridges/software/Upl_iMOD5.py
@@ -2,7 +2,7 @@ import WS_Mdl.utils as U
 
 print(U.pre_Sign)
 
-l_F = ['software/installers', 'software/iMOD5']
+l_F = ['software/iMOD5']
 
 print(f'Uploading:\n{"\n".join([f"{i:2}/{len(l_F)} - {j}" for i, j in enumerate(l_F, 1)])}\nfolder(s) to iBridges...\n')
 
