b72969318baf5bc394e57a1d9a1091446208ddbb
Marios <m.karampasis@uu.nl>
2026-01-21 13:56:30 +0100

    Transferring intallers+iMOD5 to YoDa

---
+++ code/Py/iBridges/software/Dl_iMOD5.py
@@ -0,0 +1,14 @@
+import WS_Mdl.utils as U
+
+print(U.pre_Sign)
+
+l_F = ['software/iMOD5']
+
+print(f'Downloading "{l_F}" to from iBridges...\n')
+
+S = U.iB_load_session()
+
+for F in l_F:
+    U.iB_Dl(F, S)  # , overwrite=False)
+
+print(U.post_Sign)
