a5cc8784005d43234d391c990ec8b475c2c30cda
Ydrolog <m.karampasis@uu.nl>
2026-02-17 23:01:38 +0100

    pixi launcher improvements

--- code/build/launch_PS_pixi.bat
+++ code/build/launch_PS_pixi.bat
@@ -4,4 +4,19 @@ if not defined target set "target=%CD%"
 for %%I in ("%target%") do set "target=%%~fI"
 if "%target:~-1%"=="\" set "target=%target%."
 
-start "" wt.exe -d "%target%" -- pwsh.exe -NoLogo -NoProfile -NoExit -File "G:\code\build\launch_PS_pixi.ps1"
+set "WT=%LocalAppData%\Microsoft\WindowsApps\wt.exe"
+set "PS1=%~dp0launch_PS_pixi.ps1"
+
+if not exist "%WT%" (
+  echo wt.exe not found at: "%WT%"
+  pause
+  exit /b 1
+)
+
+if not exist "%PS1%" (
+  echo PS1 not found at: "%PS1%"
+  pause
+  exit /b 1
+)
+
+"%WT%" -d "%target%" -- pwsh.exe -NoLogo -NoProfile -NoExit -File "%PS1%"
--- code/build/launch_PS_pixi_noWT.bat
+++ code/build/launch_PS_pixi_noWT.bat
@@ -4,4 +4,4 @@ if not defined target set "target=%CD%"
 for %%I in ("%target%") do set "target=%%~fI"
 if "%target:~-1%"=="\" set "target=%target%."
 
-start "" pwsh.exe -NoLogo -NoProfile -NoExit -WorkingDirectory "%target%" -File "G:\code\build\launch_PS_pixi.ps1"
+start "" pwsh.exe -NoLogo -NoProfile -NoExit -WorkingDirectory "%target%" -File ".\launch_PS_pixi.ps1"
