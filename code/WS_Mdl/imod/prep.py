31899cb501141e209d49c911ce2f517444d68dc7
Marios <m.karampasis@uu.nl>
2026-04-14 11:02:21 +0200

    #auto NBr60 env snapshot - 2026-04-14 11:02:21

--- code/WS_Mdl/imod/prep.py
+++ code/WS_Mdl/imod/prep.py
@@ -44,7 +44,7 @@ def Sim(
     M.Pa.MF6_DLL = Pa_MF6_DLL if Pa_MF6_DLL else M.Pa.MF6_DLL  # If not specified, the default location will be used.
     M.Pa.MSW_DLL = Pa_MSW_DLL if Pa_MSW_DLL else M.Pa.MSW_DLL
     M.verbose = verbose
-    M.Bin_Ins
+    M.Bin_Ins = Bin_Ins
     sprint('🟢', verbose_in=True, verbose_out=verbose, print_time=True)
 
     # %% Load PRJ & regrid it to Mdl area
